71629d93b11ae1335e54ab534891001d7eddd0ca
Canitas <r3xed@outlook.es>
2023-11-01 19:00:20 -0700

    Desarrollo en el Frontend y backend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+
   darkMode: 'class',  // This will help with implementing dark mode later on
   variants: {
     extend: {},
   },
-  plugins: [ require('postcss-nested')],
+  plugins: [ require('postcss-nested'), // Esto debe venir primero
+    require('tailwindcss'),],
   theme: {
     extend: {
       colors: {
